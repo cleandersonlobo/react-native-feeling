71f1ade3343dc54ec9fc6b056ef91e33709c53b9
Cleanderson Lobo <cleandersonlobo@gmail.com>
2020-06-25 01:51:43 -0300

    Enable Screens (React-Navigation v5)

--- index.tsx
+++ index.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { AppRegistry } from 'react-native';
+import { enableScreens } from 'react-native-screens';
 import App from './src/App';
 import { name as appName } from './app.json';
 
+enableScreens();
 require('react-native').unstable_enableLogBox();
 
 AppRegistry.registerComponent(appName, () => App);
